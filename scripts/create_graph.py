e0c09d80bf317a08f9486c56e4b51dc803b108fd
Jacob Mathias Schreiner <masc@pf574y38.usr.dmi.dk>
2025-03-13 15:57:49 +0100

    add create_graph script

---
+++ scripts/create_graph.py
@@ -0,0 +1,16 @@
+from neural_lam import create_graph
+
+
+def main(args):
+    __import__("pdb").set_trace()  # TODO delme
+    pass
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("datastore")
+    args = parser.parse_args()
+
+    main(args)
