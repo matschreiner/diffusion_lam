19f27edf5c6c0c70264ddfb804084ba5310e20d9
Jacob Mathias Schreiner <masc@pf574y38.usr.dmi.dk>
2025-03-13 15:58:23 +0100

    add save and load functionality to utlis

--- diffusion_lam/utils/utils.py
+++ diffusion_lam/utils/utils.py
@@ -1,3 +1,6 @@
+import os
+import pickle as pkl
+
 import yaml
 
 from diffusion_lam.utils.attrdict import AttrDict
@@ -7,3 +10,15 @@ def load_yaml_as_attrdict(path):
     with open(path, "r") as file:
         d = yaml.safe_load(file)
         return AttrDict(d)
+
+
+def save(obj, name):
+    name = "./" + name if not name.startswith("/") else name
+    os.makedirs(os.path.dirname(name), exist_ok=True)
+    with open(f"{name}", "wb") as f:
+        pkl.dump(obj, f)
+
+
+def load(name):
+    with open(f"{name}", "rb") as f:
+        return pkl.load(f)
