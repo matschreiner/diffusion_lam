c7be8e40d86e2aa23b9d8848f1fb0a7c9169bf82
Jacob Mathias Schreiner <masc@pf574y38.usr.dmi.dk>
2025-03-18 09:51:47 +0100

    add dataset

---
+++ dlam/data.py
@@ -0,0 +1,58 @@
+import matplotlib.pyplot as plt
+import torch
+import xarray as xr
+from torch.utils.data import Dataset
+
+#
+#  from torch
+#
+#  import yaml
+#
+#  datastore_path = "/home/masc/mllam/diffusion-lam/experiments/test/danra.datastore.yaml"
+#  yaml_datastore = yaml.safe_load(open(datastore_path, "r"))
+#  pprint(yaml_datastore)
+
+
+class Dataset(Dataset):
+    def __init__(self, selection={}, iselection{}):
+
+        data = xr.open_zarr("storage/danra_sample/height_levels.zarr")
+        data = data.sel(**selection)
+        data = data.isel(**iselection)
+        self.data = data
+
+        __import__("pdb").set_trace() #TODO delme kj:w
+
+        self.data = xr.open_zarr("storage/danra_sample/height_levels.zarr").isel(
+            **slicing
+        )
+
+    def stack_all(self, frame):
+        variables = ["u", "v"]
+        stacked_vars = []
+
+        for var in variables:
+            data = frame[var]
+            feature_dims = data.dims[:-2]
+            stacked = data.stack(feature=feature_dims)
+
+            stacked_vars.append(stacked)
+        return xr.concat(stacked_vars, dim="feature")
+
+    def __len__(self):
+        return len(self.data.time)
+
+    def __getitem__(self, idx):
+        frame = self.data.isel(time=idx, **self.slicing)
+        frame = self.stack_all(frame)
+
+        return frame
+
+
+ds = Dataset()
+
+for i in range(10):
+    for idx in range(len(ds)):
+        print(i, idx)
+        #  a = ds[idx]
+        #  print(a)
