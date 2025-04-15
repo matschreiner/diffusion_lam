c7ddf39897650cc22adf5310e852b985b8485d74
Jacob Mathias Schreiner <masc@pf574y38.usr.dmi.dk>
2025-04-15 11:13:29 +0200

    add callback

--- scripts/train.py
+++ scripts/train.py
@@ -1,39 +1,55 @@
 import os
+from datetime import timedelta
 from pprint import pprint
 
-import matplotlib.pyplot as plt
-from pytorch_lightning.profilers import SimpleProfiler
+import pytorch_lightning as pl
 from torch.utils.data import DataLoader
 
+import dlam
 from dlam import utils
 from dlam.trainer import Trainer
-from dlam.vis import animate
 
 
 def main(config):
+    pprint(config)
     dataset = utils.get_component(config.dataset)
     dataloader = DataLoader(dataset, **config.dataloader.get("kwargs", {}))
 
-    profiler_dir_path = "."
-    profiler_filename = "profiler"
+    loss_checkpoint_callback = pl.callbacks.ModelCheckpoint(
+        dirpath=f"{config.path}/checkpoints",
+        filename="best",
+        monitor="loss",
+        mode="min",
+        save_last=True,
+    )
+    timedelta_checkpoint_callback = pl.callbacks.ModelCheckpoint(
+        dirpath=f"{config.path}/checkpoints",
+        filename="timedelta",
+        train_time_interval=timedelta(seconds=60),
+    )
+
     trainer = Trainer(
         config.trainer.get("scheduler_config", {}),
         config.trainer.get("optimizer_config", {}),
         **config.trainer.get("kwargs", {}),
-        profiler=SimpleProfiler(dirpath=profiler_dir_path, filename=profiler_filename),
-        accelerator="gpu",
+        accelerator=dlam.DEVICE,
         devices=1,
+        callbacks=[loss_checkpoint_callback, timedelta_checkpoint_callback],
+        log_every_n_steps=1,
     )
 
-    noise_model = utils.get_component(config.noise_model)
-    model = utils.get_component(config.score_based_model, noise_model=noise_model)
-    model.to("cuda")
+    if "score_base_model" in config:
+        noise_model = utils.get_component(config.noise_model).to(dlam.DEVICE)
+        model = utils.get_component(config.score_based_model, noise_model=noise_model)
+
+    elif "model" in config:
+        model = utils.get_component(config.model, domain=dataset.xy)
+
+    model.to(dlam.DEVICE)
 
     trainer.fit(model, dataloader)
 
     os.makedirs("results", exist_ok=True)
-    utils.save(model.cpu(), "results/final_model.pkl")
-    utils.get_component(config.evaluation, model=model, dataset=dataset)
 
 
 if __name__ == "__main__":
@@ -44,5 +60,6 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     config = utils.load_yaml(args.train_config)
+    config.path = os.path.dirname(args.train_config)
 
     main(config)
