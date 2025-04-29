81494473c453f5839c75814cd02262312ad27abc
Jacob Mathias Schreiner <masc@pf574y38.usr.dmi.dk>
2025-04-29 15:58:39 +0200

    add logger

--- dlam/__init__.py
+++ dlam/__init__.py
@@ -1,8 +1,8 @@
-#  import lovely_tensors as lt
+import lovely_tensors as lt
 import torch
 
-from dlam import data, model, samplers, scheduler, trainer
+from dlam import data, logger, model, samplers, scheduler, trainer
 
-#  lt.monkey_patch()
+lt.monkey_patch()
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
