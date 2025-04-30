8610f915c1f4ff6a6bb26d96133b3b13c7027a07
Jacob Mathias Schreiner <masc@pf574y38.usr.dmi.dk>
2025-04-30 13:10:52 +0200

    test

--- test.py
+++ test.py
@@ -1,30 +1,29 @@
-import numpy as np
-import xarray as xr
+import os
 
-a_values = np.arange(2, 8).reshape(2, 3) / 10
-b_values = np.arange(28, 34).reshape(2, 3) / 10
+# Standard library
+import sys
 
-ds = xr.Dataset(
-    {
-        "a": (["x", "y"], a_values),  # 'a' variable with dimensions ['x', 'y']
-        "b": (["x", "y"], b_values),  # 'b' variable with dimensions ['x', 'y']
-    },
-    coords={
-        "x": [1, 2],  # x coordinates
-        "y": [10, 20, 30],  # y coordinates
-    },
-)
+import mlflow
+import mlflow.pytorch
+import pytorch_lightning as pl
+import xarray
+
+import dlam
 
 
-#  feature = xr.concat([ds["a"], ds["b"]], dim="y")
-#
-#  feature = feature.rename({"y": "feature_dim"})
-#  feature = feature.drop("feature_dim")
-#
-#
-#  new_ds = xr.Dataset(
-#      {"feature": feature},  # Add the concatenated data as a variable in the new dataset
-#  )
+def main():
+    da = xarray.open_zarr(
+        "/dmidata/projects/cloudphysics/danra/data/v0.5.0/height_levels.zarr"
+    )
 
-#  new_ds = new_ds.rename({"y": "feature_dim"})
-__import__("pdb").set_trace()  # TODO delme
+    __import__("pdb").set_trace()  # TODO delme
+    # da_stacked.dims == ('time', 'feature')
+    # da_stacked.shape == (96768, 4*9*589*789)
+
+    # 3) (optional) transpose if you want features first and time second:
+    da_stacked = da_stacked.transpose("feature", "time")
+
+    __import__("pdb").set_trace()  # TODO delme
+
+
+main()
